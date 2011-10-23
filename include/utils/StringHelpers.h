333c824d5d219cca2839f6407f969f98e142ae7c
Sascha Liebsch <sascha@gruenes-huhn.de>
2011-10-24 01:26:11 +0200

    Add string helper method that allow to specify float precision

--- include/utils/StringHelpers.h
+++ include/utils/StringHelpers.h
@@ -2,6 +2,7 @@
 #define GW1K_STRINGHELPERS_H_
 
 #include <sstream>
+#include <iomanip>
 #include <string>
 
 namespace gw1k
@@ -14,6 +15,16 @@ toString(T arg)
 {
     std::stringstream ss;
     ss << arg;
+    return ss.str();
+}
+
+
+template<typename T>
+inline std::string
+toString(T arg, int precision)
+{
+    std::stringstream ss;
+    ss << std::fixed << std::setprecision(precision) << arg;
     return ss.str();
 }
 
