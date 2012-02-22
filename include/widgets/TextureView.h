ElkMonster/gw1k

commit 8cfc523262ed3da9416ba83726c76e83c85bf995

TextureView:  - Initialise all members with useful values  - Handle setSize() correctly if auto adaptation is enabled, but no texture loaded (sucessfully)

src/widgets/TextureView.cpp
@@ -20,6 +20,7 @@ TextureView::TextureView(
 :   OGLView(pos, size, shadeColorScheme),
     filename_(filename),
     pTex_(0),
+    imgSize_(0, 0),
     pImgData_(0),
     bReqLoadTexture_(false),
     aspectRatioAutoAdapt_(AR_NO_ADAPT)
@@ -61,6 +62,7 @@ TextureView::loadTexture(const std::string& filename)
     {
         glDeleteTextures(1, pTex_);
         delete pTex_;
+        pTex_ = 0;
     }
 
     if (loadPngTexture(filename.c_str(), imgSize_.x, imgSize_.y, imgAlpha_, pImgData_))
@@ -174,19 +176,33 @@ TextureView::setSize(float width, float height)
     // Get pixel-size first
     const Point& newSize = OGLView::setSize(width, height);
 
+    if (!pTex_ && aspectRatioAutoAdapt_ != AR_NO_ADAPT)
+    {
+        Log::warning("TextureView", Log::os()
+            << "No size auto-adaptation due to missing texture");
+        return newSize;
+    }
+
     switch (aspectRatioAutoAdapt_)
     {
     case AR_ADAPT_WIDTH:
     {
-        float w = static_cast<float>(newSize.y * imgSize_.x) / imgSize_.y;
-        return OGLView::setSize(w, newSize.y);
+        if (pTex_)
+        {
+            float w = static_cast<float>(newSize.y * imgSize_.x) / imgSize_.y;
+            return OGLView::setSize(w, newSize.y);
+        }
     }
     case AR_ADAPT_HEIGHT:
     {
-        float h = static_cast<float>(newSize.x * imgSize_.y) / imgSize_.x;
-        return OGLView::setSize(newSize.x, h);
+        if (pTex_)
+        {
+            float h = static_cast<float>(newSize.x * imgSize_.y) / imgSize_.x;
+            return OGLView::setSize(newSize.x, h);
+        }
     }
     case AR_NO_ADAPT:
+    default:
         break;
     }
 

commit bec0f7ef06a9ea6fe4a1f3a729bd267ef05f7729

TextureView: Don't delay texture creation; this fixes an issue introduced by previous commit (8cfc523262ed3da94) which generally breaks auto-sizing for TextureViews whose auto size property is set before any render step (since texture creation only happened in preRenderUpdate())

include/widgets/TextureView.h
@@ -53,16 +53,11 @@ public:
     void setAspectRatioAutoResize(AspectRatioAutoAdapt a);
 
     /**
-     * Loads the given image file and enqueues this TextureView in WManager's
-     * preRenderUpdate() list. Thus, a texture will only be created from the
-     * image when the OpenGL state is ready for it.
-     * (See http://www.opengl.org/wiki/Common_Mistakes for details)
+     * Loads the given image file and generates a texture for it.
      */
     bool loadTexture(const std::string& filename);
 
     const Point& getTextureSize() const;
-
-    virtual void preRenderUpdate();
 
     virtual void renderOGLContent() const;
 
@@ -88,9 +83,6 @@ private:
     bool imgAlpha_;
 
     unsigned char* pImgData_;
-
-    /** If this is true, a call to preRenderUpdate() will create the texture */
-    bool bReqLoadTexture_;
 
     AspectRatioAutoAdapt aspectRatioAutoAdapt_;
 };

src/widgets/TextureView.cpp
@@ -22,7 +22,6 @@ TextureView::TextureView(
     pTex_(0),
     imgSize_(0, 0),
     pImgData_(0),
-    bReqLoadTexture_(false),
     aspectRatioAutoAdapt_(AR_NO_ADAPT)
 {
     loadTexture(filename);
@@ -78,8 +77,7 @@ TextureView::loadTexture(const std::string& filename)
             }
         }
 
-        bReqLoadTexture_ = true;
-        WManager::getInstance()->registerForPreRenderUpdate(this);
+        createTexture();
         return true;
     }
     return false;
@@ -121,17 +119,6 @@ const Point&
 TextureView::getTextureSize() const
 {
     return imgSize_;
-}
-
-
-void
-TextureView::preRenderUpdate()
-{
-    if (bReqLoadTexture_)
-    {
-        createTexture();
-        bReqLoadTexture_ = false;
-    }
 }
 
 